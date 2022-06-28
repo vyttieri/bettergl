b2db6734d6a00dff6b7689d0dc5c37c7ef0c6982
deakolt <alexandervaccaro@gmail.com>
2022-06-28 10:03:29 -0700

    React working with htm templates

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,27 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import '@hotwired/turbo-rails'
+import 'controllers'
+import 'components/form'
+import h from 'helpers/createElement'
+
+import React from 'react'
+import { render } from 'react-dom'
+
+const App = props => (
+	h`
+	<div>
+		Test ${props.name}
+	</div>
+	`
+)
+
+App.defaultProps = {
+	name: 'Whatever'
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+	render(
+		h`<${App} name='htm' />`,
+		document.getElementById('app')
+	)
+})
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,10 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "react", to: "https://ga.jspm.io/npm:react@18.2.0/index.js"
+pin "react-dom", to: "https://ga.jspm.io/npm:react-dom@18.2.0/index.js"
+pin "process", to: "https://ga.jspm.io/npm:@jspm/core@2.0.0-beta.24/nodelibs/browser/process-production.js"
+pin "scheduler", to: "https://ga.jspm.io/npm:scheduler@0.23.0/index.js"
+pin_all_from "app/javascript/components", under: "components"
+pin_all_from "app/javascript/helpers", under: "helpers"
+pin "htm", to: "https://ga.jspm.io/npm:htm@3.1.1/dist/htm.module.js"
