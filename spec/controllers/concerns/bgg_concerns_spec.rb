f9f0c8f690d3ecce130117022713734e1d5497a1
deakolt <alexandervaccaro@gmail.com>
2022-06-29 13:02:58 -0700

    Move concerns spec to correct folder

--- spec/controllers/concerns/bgg_concerns_spec.rb
+++ spec/controllers/concerns/bgg_concerns_spec.rb
@@ -1,13 +1,22 @@
+require 'rails_helper'
+
 RSpec.describe 'Bgg_concerns' do
+	let(:test_class) { Struct.new(:url) { include BggConcerns } }
+
 	describe 'fetch_list' do
 		context 'with valid geeklist url' do
+			let(:bgg_concerns) { test_class.new('url') }
+
 			it 'successfully fetches the html' do
+				html = bgg_concerns.fetch_list('asdf')
 			end
 		end
 	end
 
 	describe 'parse_list' do
 		context 'with valid html' do
+			let(:bgg_concerns) { test_class.new('url') }
+
 			it 'successfully parses data fields' do
 			end
 		end
