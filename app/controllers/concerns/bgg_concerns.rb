59160afc5947f00233cd470f2239904a5d902c3c
deakolt <alexandervaccaro@gmail.com>
2022-06-29 14:35:58 -0700

    Use Vessel for scraping

--- app/controllers/concerns/bgg_concerns.rb
+++ app/controllers/concerns/bgg_concerns.rb
@@ -1,18 +1,21 @@
-require 'open-uri'
-
 module BggConcerns
-	extend ActiveSupport::Concern
+  extend ActiveSupport::Concern
 
-	def fetch_list(url)
-		begin
-			html = URI.open(url)
-		end
+  class GeeklistScraper < Vessel::Cargo
+    domain 'boardgamegeek.com'
+    start_urls 'https://boardgamegeek.com/geeklist/298162/beautiful-fillers'
 
-		html
-	end
+    def parse
+      css('gg-geeklist-item-ui').each do |geeklist_item|
+        yield({
+          text: geeklist_item
+        })
+      end
 
-
-	def parse_html(html)
-		doc = Nokogiri::HTML(html)
-	end
+      # if next_page = at_xpath("//li[@class='next']/a[@href]")
+      #   url = absolute_url(next_page.attribute(:href))
+      #   yield request(url: url, handler: :parse)
+      # end
+    end
+  end
 end
--- spec/controllers/concerns/bgg_concerns_spec.rb
+++ spec/controllers/concerns/bgg_concerns_spec.rb
@@ -2,37 +2,39 @@ require 'rails_helper'
 require 'spec_helper'
 
 RSpec.describe 'Bgg_concerns' do
-	let(:test_class) { Struct.new(:url) { include BggConcerns } }
+  let(:test_class) { Struct.new(:url) { include BggConcerns } }
 
-	describe 'fetch_list' do
-		context 'with valid geeklist url' do
-			let(:bgg_concerns) { test_class.new('https://boardgamegeek.com/geeklist/298162/beautiful-fillers') }
-			let(:html) do
-				VCR.use_cassette('geeklist', record: :new_episodes) do
-					bgg_concerns.fetch_list(bgg_concerns.url)
-				end
-			end
+  describe 'fetch_list' do
+    context 'with valid geeklist url' do
+      let(:bgg_concerns) { test_class.new('https://boardgamegeek.com/geeklist/298162/beautiful-fillers') }
+      let(:html) do
+        quotes = []
+        VCR.use_cassette('geeklist', record: :new_episodes) do
+          BggConcerns::GeeklistScraper.run { |q| quotes << q }
+        end
+        puts JSON.generate(quotes)
+      end
 
-			it 'successfully fetches the html' do
-				puts html
-			end
-		end
+      it 'successfully fetches the html' do
+        puts html
+      end
+    end
 
-		context 'with invalid geeklist url' do
+    context 'with invalid geeklist url' do
 
-		end
-	end
+    end
+  end
 
-	describe 'parse_list' do
-		context 'with valid html' do
-			let(:bgg_concerns) { test_class.new('url') }
+  describe 'parse_list' do
+    context 'with valid html' do
+      let(:bgg_concerns) { test_class.new('url') }
 
-			it 'successfully parses data fields' do
-			end
-		end
+      it 'successfully parses data fields' do
+      end
+    end
 
-		context 'with invalid html' do
+    context 'with invalid html' do
 
-		end
-	end
+    end
+  end
 end
