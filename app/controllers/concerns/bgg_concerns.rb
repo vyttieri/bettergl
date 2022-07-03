3379bae1e81fb37433662297419361e9ece277c7
deakolt <alexandervaccaro@gmail.com>
2022-07-03 16:20:21 -0700

    Add basic url validation to GeeklistScraper

--- app/controllers/concerns/bgg_concerns.rb
+++ app/controllers/concerns/bgg_concerns.rb
@@ -5,6 +5,8 @@ module BggConcerns
     attr_reader :geeklist_url, :geeklist_items
 
     def initialize(geeklist_url)
+      validate_geeklist_url! geeklist_url
+
       @geeklist_url = geeklist_url
       @geeklist_items = []
       @browser = Ferrum::Browser.new(timeout: 30)
@@ -15,6 +17,11 @@ module BggConcerns
       sleep 2
 
       pages = @browser.css('nav gg-pagination-input > ul > li > p > span')[0].text.split(' ')[1].to_i
+
+      # Need to visit the first page in the geeklist to get a count of pages to scrape,
+      # so after doing that, we parse the first page here before entering the loop
+      parse_page
+
       cur_page = 2
       while cur_page <= pages
         new_url = @geeklist_url + "?page=#{cur_page}"
@@ -42,7 +49,12 @@ module BggConcerns
 
         @geeklist_items << {rank: rank, title: title, rating: rating, link: link}
       end
+    end
 
+    def validate_geeklist_url!(geeklist_url)
+      if /\Ahttps:\/\/boardgamegeek.com\/geeklist\/\d+\/[a-z\-]*\z/.match(geeklist_url).nil?
+        raise ArgumentError, "Invalid Geeklist URL: #{geeklist_url}"
+      end
     end
   end
 end
