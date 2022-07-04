6c270f41bba6059ae30f22c14c188c2741e1ae41
deakolt <alexandervaccaro@gmail.com>
2022-07-04 10:25:54 -0700

    Add table headers

--- app/javascript/components/list.js
+++ app/javascript/components/list.js
@@ -3,18 +3,28 @@ import h from 'helpers/createElement'
 
 
 const List = ({ geeklistItems }) => {
-	return (
-		h`
-			<table><tbody>
+	return (h`
+		<table>
+			<thead>
+				<tr>
+					<td>Rank</td>
+					<td>Title</td>
+					<td>Rating</td>
+					<td>BGG Link</td>
+				</tr>
+			</thead>
+			<tbody>
 		${geeklistItems.map(geeklistItem =>
-			h`<tr>
-				<td>${geeklistItem.rank}</td>
-				<td>${geeklistItem.title}</td>
-				<td>${geeklistItem.rating}</td>
-				<td>${geeklistItem.link}</td>
-			</tr>`
+			h`
+				<tr>
+					<td>${geeklistItem.rank}</td>
+					<td>${geeklistItem.title}</td>
+					<td>${geeklistItem.rating}</td>
+					<td>${geeklistItem.link}</td>
+				</tr>`
 		)}
-		</tbody></table>`
+			</tbody>
+		</table>`
 	)
 }
 
